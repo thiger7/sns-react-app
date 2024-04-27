7511803e51ce258cbc1101dd42e25d0d35c823ce
thiger7 <adf0423@gmail.com>
2024-04-27 22:01:12 +0900

    ユーザーのログイン機能を作成

--- src/repositories/auth.js
+++ src/repositories/auth.js
@@ -10,4 +10,16 @@ export const authRepository = {
     if (error != null) throw new Error(error.message);
     return { ...data.user, userName: data.user.user_metadata.name };
   },
+  async signin(email, password) {
+    const {data, error} = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if(error) throw new Error(error.message);
+    return {
+      ...data.user,
+      userName: data.user.user_metadata.name,
+    };
+  },
 };
+
