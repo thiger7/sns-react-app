2eda8028b6d33bfd3b8f07a77d1b64e052ae6069
thiger7 <adf0423@gmail.com>
2024-05-02 22:17:10 +0900

    ログイン状態を保持できるようにする

--- src/SessionProvider.js
+++ src/SessionProvider.js
@@ -1,8 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { authRepository } from "./repositories/auth";
 
 const SessionContext = createContext();
 const SessionProvider = props => {
   const [currentUser, setCurrentUser] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setSession();
+  }, []);
+
+  const setSession = async () => {
+    const currentUser = await authRepository.getCurrentUser();
+    setCurrentUser(currentUser);
+    setIsLoading(false);
+  };
+
+  if (isLoading) return <div />;
 
   return (
     <SessionContext.Provider value={{ currentUser, setCurrentUser }}>
@@ -11,4 +24,4 @@ const SessionProvider = props => {
   );
 };
 
-export { SessionContext, SessionProvider }
+export { SessionContext, SessionProvider };
--- src/repositories/auth.js
+++ src/repositories/auth.js
@@ -11,15 +11,24 @@ export const authRepository = {
     return { ...data.user, userName: data.user.user_metadata.name };
   },
   async signin(email, password) {
-    const {data, error} = await supabase.auth.signInWithPassword({
+    const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
     });
-    if(error) throw new Error(error.message);
+    if (error) throw new Error(error.message);
     return {
       ...data.user,
       userName: data.user.user_metadata.name,
     };
   },
-};
+  async getCurrentUser() {
+    const { data, error } = await supabase.auth.getSession();
+    if (error != null ) throw new Error(error.message);
+    if (data.session == null) return null;
 
+    return {
+      ...data.session.user,
+      userName: data.session.user.user_metadata.name,
+    }
+  },
+};
